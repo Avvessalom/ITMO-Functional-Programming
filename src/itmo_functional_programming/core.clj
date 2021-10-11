063a7786fa2ae4c69828cf359d8e8b0ce57677ad
avvessalom <avvessalom@gmail.com>
2021-10-11 19:51:49 +0300

    first try

--- src/itmo_functional_programming/core.clj
+++ src/itmo_functional_programming/core.clj
@@ -1,6 +1,20 @@
 (ns itmo-functional-programming.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+;
+;What is the 10 001st prime number?
+
+(defn sqrt-range [n]
+  (take-while #(>= n (* % %)) (range 2 1.0E15)))
+
+(defn is-prime? [n]
+  (if (< n 2)
+    false
+    (let [s (sqrt-range n)]
+      (every? true? (map #(not= 0 (rem n %)) s))
+      )))
+
+(defn primes [num]
+  (time (last (take num (filter is-prime? (range)))))
+  )
+
