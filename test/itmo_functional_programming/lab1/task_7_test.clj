a385535688192268891237f6ac2d3a430d986f05
avvessalom <avvessalom@gmail.com>
2021-10-24 20:46:04 +0300

    update tests

--- test/itmo_functional_programming/lab1/task_7_test.clj
+++ test/itmo_functional_programming/lab1/task_7_test.clj
@@ -4,8 +4,15 @@
 
 (deftest seven-task-loop-test
   (testing "7-th task, loop."
-    (is (= (n-th-prime-number-loop 10001) 104743))))
+    (is (= (n-th-prime-number-loop 10001) 104743))
+    (is (= (n-th-prime-number-loop 1) 2))
+    (is (= (n-th-prime-number-loop 10) 29))
+    (is (= (n-th-prime-number-loop 100) 541))))
 
 (deftest seven-task-filter-test
   (testing "7-th task, filter"
-    (is (= (n-th-prime-number-filtered 10001) 104743))))
+    (is (= (n-th-prime-number-filtered 10001) 104743))
+    (is (= (n-th-prime-number-filtered 1) 2))
+    (is (= (n-th-prime-number-filtered 10) 29))
+    (is (= (n-th-prime-number-filtered 100) 541))
+    ))
