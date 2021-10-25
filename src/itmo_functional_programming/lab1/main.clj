f7bb57efe363b5f4b79ad57c6718eb22dc4828db
avvessalom <avvessalom@gmail.com>
2021-10-25 21:02:13 +0300

    add task 24 solution

--- src/itmo_functional_programming/lab1/main.clj
+++ src/itmo_functional_programming/lab1/main.clj
@@ -3,4 +3,5 @@
             [itmo-functional-programming.lab1.task-24 :refer [task-24-report]]))
 
 (defn -main [] (do
-                 (task-7-report)))
+                 (task-7-report)
+                 (task-24-report)))
---
+++ src/itmo_functional_programming/lab1/task_24.clj
@@ -0,0 +1,31 @@
+(ns itmo-functional-programming.lab1.task-24
+  "Task 24 from Project Euler:
+    - A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+    - If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+    - 012   021   102   120   201   210
+    - What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+  "
+  )
+
+(def digits (sorted-set 0 1 2 3 4 5 6 7 8 9))
+
+(defn next-lex [substr digits]
+  (let [lex (apply str substr digits)]
+    (lazy-cat
+      [lex]
+      (loop [s (vec lex)
+             d (sorted-set)]
+        (let [i (- (int (peek s)) 48)                       ; code of nine
+              d (conj d i)]
+          (if (== (last d) i)
+            (recur (pop s) d)
+            (let [z (first (drop-while #(<= % i) d))]
+              (next-lex (apply str (conj (pop s) (char (+ z 48)))) (disj d z)))))))))
+
+(defn lex-perm-stupid [num]
+  (first (drop (dec num) (next-lex "" digits))))
+
+(defn task-24-report []
+  (do
+    (println (format "Task 24 solutions:
+                * stupid:   %s" (lex-perm-stupid 1000000)))))
