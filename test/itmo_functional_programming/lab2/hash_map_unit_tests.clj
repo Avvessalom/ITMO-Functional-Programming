7f9003cf7a0cf3d065d4245ed27c5490d9d02425
avvessalom <avvessalom@gmail.com>
2021-12-07 22:46:02 +0300

    add hash-map tests

---
+++ test/itmo_functional_programming/lab2/hash_map_unit_tests.clj
@@ -0,0 +1,51 @@
+(ns itmo-functional-programming.lab2.hash-map-unit-tests
+  (:require [clojure.test :refer :all]
+            [itmo-functional-programming.lab2.hash-map :refer :all]))
+
+
+(deftest my-map-test
+  (let [map-for-test (my-hash-map {1 2 3 4})
+        fake-map (my-hash-map {5 6 7 8})]
+    (is (= map-for-test (my-hash-map {1 2 3 4})))
+    (is (not= map-for-test fake-map))))
+
+(deftest map-first-test
+  (let [map-for-test (my-hash-map {1 2 3 4})
+        true-map (hash-map 1 2 3 4)
+        fake-map (my-hash-map {5 6 7 8})]
+    (is (= (first map-for-test) (first true-map)))
+    (is (not= (first map-for-test) (first fake-map)))))
+
+(deftest map-count-test
+  (let [empty-map (my-hash-map)
+        map-for-test (my-hash-map {1 2 3 4})]
+    (is (= 0 (count empty-map)))
+    (is (not= 1 (count empty-map)))
+    (is (= 2 (count map-for-test)))
+    (is (not= 0 (count map-for-test)))))
+
+(deftest map-next-test
+  (let [map-for-test (my-hash-map {1 2 3 4})
+        true-map (hash-map 1 2 3 4)
+        fake-map (my-hash-map {5 6 7 8})]
+    (is (= (next map-for-test) (next true-map)))
+    (is (not= (next map-for-test) (next fake-map)))))
+
+(deftest map-more-test
+  (let [map-for-test (my-hash-map {1 2 3 4})
+        true-map (hash-map 1 2 3 4)
+        fake-map (my-hash-map {5 6 7 8})]
+    (is (= (rest map-for-test) (rest true-map)))
+    (is (not= (rest map-for-test) (rest fake-map)))))
+
+(deftest map-empty-test
+  (let [map-for-test (my-hash-map {1 2 3 4})
+        empty-map (my-hash-map)]
+    (is (= (empty map-for-test) empty-map))
+    (is (not= (empty map-for-test) map-for-test))))
+
+(deftest map-seq-test
+  (let [map-for-test (my-hash-map {1 2 3 4})
+        true-map (hash-map 1 2 3 4)]
+    (is (= (seq map-for-test) (seq true-map)))
+    (is (= (seq map-for-test) (list 1 2)))))
