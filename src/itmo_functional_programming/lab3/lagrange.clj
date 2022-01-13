bef721f9e69b0b6c2c6bb69179c7b524ca7761f5
avvessalom <avvessalom@gmail.com>
2022-01-13 20:41:39 +0300

    mb finish lagrange

--- src/itmo_functional_programming/lab3/lagrange.clj
+++ src/itmo_functional_programming/lab3/lagrange.clj
@@ -1,27 +1,24 @@
 (ns itmo-functional-programming.lab3.lagrange)
 
-(defn lagrange-polint [points]
+(defn numerator-f [i x s]
+  (reduce-kv #(if (== i %2)
+                %1
+                (* %1 (- x %3)))
+             1
+             s))
+
+(defn xs-denom
+  [x y xs]
+  (mapv (fn [x y]
+          (/ y (reduce #(if (== x %2)
+                          %1
+                          (* %1 (- x %2))) 1 xs)))
+        x y))
+
+(defn lagrange [points]
   (let [xs (mapv first points)
-        ys (mapv second points)
-        xs-denom (mapv (fn [x y]
-                         (/ y
-                            (reduce (fn [r xn]
-                                      (if (== x xn)
-                                        r
-                                        (* r (- x xn))))
-                                    1
-                                    xs)))
-                       xs
-                       ys)
-        numerator-f (fn [i x]
-                      (reduce-kv (fn [r j xj]
-                                   (if (== i j)
-                                     r
-                                     (* r (- x xj))))
-                                 1
-                                 xs))]
+        ys (mapv second points)]
     (fn [x]
       (reduce +
-              (map-indexed (fn [i dn] (* (numerator-f i x) dn))
-                           xs-denom)))))
-(mapv (lagrange-polint [[1 1] [2 8] [3 27] [6 216]]) [2.1 5])
+              (map-indexed #(* (numerator-f %1 x xs) %2)
+                           (xs-denom xs ys xs))))))
